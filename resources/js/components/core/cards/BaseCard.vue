bba7e47edb272a5053490094335ff8d0ae0d9609
sergiu <sergiugrigoreturcanu@gmail.com>
2025-05-19 03:13:16 -0400

    PORT-2 - Added the Experience page, not yet mobile supported for the style but we have it working and also filled the structure on the experience and what is expected to receive fro ma future API implementation in order to mkae the front-end work.

--- resources/js/components/core/cards/BaseCard.vue
+++ resources/js/components/core/cards/BaseCard.vue
@@ -1,8 +1,18 @@
 <script setup>
+  const props = defineProps({
+    classes: {
+      type: String,
+      default: ""
+    }
+  });
+
+  const defaultClasses = "border rounded-lg shadow bg-gray-800 border-gray-700 max-w-sm w-[25%] h-fit mx-8";
 </script>
 
 <template>
-  <div class="max-w-sm w-[25%] h-fit  border mx-8  rounded-lg shadow bg-gray-800 border-gray-700">
+  <div
+    :class="props.classes ? props.classes : defaultClasses"
+  >
     <slot></slot>
   </div>
 </template>
---
+++ resources/js/components/core/cards/CardHeader.vue
@@ -0,0 +1,35 @@
+<script setup>
+  const props = defineProps({
+    title: {
+      type: String,
+      default: null
+    },
+    classes: {
+      type: String,
+      default: null
+    },
+    link: {
+      type: String,
+      default: null,
+    }
+  });
+</script>
+
+<template>
+  <a
+    v-if="props.link"
+    :href="props.link"
+    target="_blank"
+    class="h2 text-4xl font-bold text-white-800 hover:text-green-500 hover:underline transition-colors duration-200 cursor-pointer"
+  >
+    {{ props.title }}
+  </a>
+  <h2
+    v-else
+    class="text-4xl font-bold text-white-800"
+    :class="props.classes"
+  >
+
+    {{ props.title }}
+  </h2>
+</template>
