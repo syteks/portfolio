0738cdf78b8c94bb758e533a39280a47ea1b3dbc
sergiu <sergiugrigoreturcanu@gmail.com>
2025-06-15 21:34:12 -0400

    MAIN : Added updated proxy for Gcloud.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,8 +3,16 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as ProxyMiddleware;
+use Illuminate\Http\Request as HttpRequest;
 
 class TrustProxies extends ProxyMiddleware
 {
-    protected $proxies = '**';
+    protected $proxies = '*';
+
+    protected $headers = HttpRequest::HEADER_X_FORWARDED_FOR |
+        HttpRequest::HEADER_X_FORWARDED_HOST |
+        HttpRequest::HEADER_X_FORWARDED_PORT |
+        HttpRequest::HEADER_X_FORWARDED_PROTO |
+        HttpRequest::HEADER_X_FORWARDED_PREFIX |
+        HttpRequest::HEADER_X_FORWARDED_AWS_ELB;
 }
